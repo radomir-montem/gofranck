76f3fe91e4c86313bc0c20cb0a8523900471e0ed
radomir-montem <radomir@montemagency.com>
2025-08-29 17:11:38 +0900

    add sticky atc and update review

---
+++ assets/sticky-atc.js
@@ -0,0 +1,30 @@
+class stickyAtc extends HTMLElement {
+    constructor() {
+        super();
+        this.productForm = document.querySelector('.sg-product-hero .sg-product-page-hero');
+        this.stickyButton = this.querySelector('button');
+        this.atcButtton = document.querySelector('.sg-product-page-button');
+        this.productInfo = document.querySelector('.sg-product-page-purchase-block');
+    }
+
+    connectedCallback() {
+        const _this = this;
+        const handleIntersection = (entries, observer) => {
+            entries.forEach(entrie => {
+                _this.classList.toggle('hidden', entrie.isIntersecting && window.scrollY > _this.productForm.getBoundingClientRect().bottom);
+                _this.classList.toggle('show', !entrie.isIntersecting && window.scrollY > _this.productForm.getBoundingClientRect().bottom);
+            });
+        }
+
+        new IntersectionObserver(handleIntersection.bind(this.productForm), { rootMargin: `-500px 0px 0px 0px` }).observe(this.productForm);
+        this.stickyButton.addEventListener('click', this.onClickAtcButton.bind(this));
+    }
+
+    onClickAtcButton(e) {
+        e.preventDefault();
+        this.productInfo.scrollIntoView({ behavior: "smooth", block: "center" });
+        this.atcButtton.focus();
+    }
+}
+
+customElements.define('sticky-atc', stickyAtc);
